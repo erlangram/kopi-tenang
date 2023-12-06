fb688ed6036ca17c538d66cbb4dd3080d17a282d
Erlangram <erlanggaramadhan424@gmail.com>
2023-12-06 15:28:53 +0700

    membuat website kopi tenang

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const navbarnav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
